ad8cd0d5163b4cca41bde19a27b5653c80468029
manea <manea@LAPTOP-VD67QN5L>
2022-07-16 00:47:04 +0530

    Add project files.

---
+++ Exchange.cpp
@@ -0,0 +1,12 @@
+﻿// Exchange.cpp : Defines the entry point for the application.
+//
+
+#include "Exchange.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
